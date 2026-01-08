7083becdd224bb5a70612ddfc2395eda15b971ba
4723208 <4723208g@hgu.jp>
2026-01-08 05:22:45 +0000

    Add delete function to Todo app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,24 @@
 <script setup lang="ts">
 import AddTodo from './components/AddTodo.vue';
+import { useTodoStore } from './stores/todoStores';
+
 
 import { ListFormat } from 'typescript';
 import { ref, computed } from 'vue';
 const name = ref('Vue 3 with TypeScript');
 
-type Todo = { id: number; title: string; completed: boolean };
-
-const todos = ref<Todo[]>([
-  { id: 1, title: 'Buy groceries', completed: false},
-  { id: 2, title: 'Write report', completed: true },
-  { id: 3, title: 'Call Alice', completed: false },  
-]);
-
-const falseTodos = computed(() => todos.value.filter((i) => i.completed === false));
-const falseTodosValue = computed(() => falseTodos.value.length);
-
-
-
+const todoStore = useTodoStore();
 
 
 </script>
 
 <template>
   <div id="app">
-    <h2>Todos({{ falseTodosValue }})</h2>
+    <h2>Todos</h2>
 
     <ul>
       <li
-        v-for="todo in todos"
+        v-for="todo in todoStore.todos"
         :key="todo.id"
         style="display:flex; align-items:center; gap:0.5rem; margin:0.25rem 0;"
       >
@@ -36,10 +26,11 @@ const falseTodosValue = computed(() => falseTodos.value.length);
         <span :style="{ textDecoration: todo.completed ? 'line-through' : 'none' }">
           {{ todo.title }}
         </span>
+        <button v-on:click="todoStore.removeTodo(todo.id)">Delete</button>
       </li>
     </ul>
 
-    <AddTodo v-model:todos="todos" />
+    <AddTodo />
 
   </div>
 
--- frontend/src/components/AddTodo.vue
+++ frontend/src/components/AddTodo.vue
@@ -1,33 +1,24 @@
 <script setup lang="ts">  
+import { useTodoStore } from '@/stores/todoStores';
 import { ref } from 'vue';
 
-type Todo = {id: number; title: string; completed: boolean };
+const todoStore = useTodoStore();
 
-const todos = defineModel<Todo[]>("todos", {required: true});
+const newTitle = ref<string>('');
 
-
-
-const newTitle = ref('');
-
-const nextId = ref(Math.max(0, ...todos.value.map((t) => t.id)) + 1);
-
-function addTodo() {
-  const title = newTitle.value;
-  todos.value.push({ id: nextId.value++, title, completed: false });
-  newTitle.value = '';
+function addTodo(){
+    todoStore.addTodo(newTitle.value);
+    newTitle.value = '';
 }
+
 </script>
 
 <template>
   <section>
     <h2>New todo</h2>
     <div style="display: flex; align-items: center; gap: 0.5rem; margin-top: 0.25rem;">
-      <input
-       v-model="newTitle"
-       placeholder="New todo title"
-       aria-label="=New todo title"
-      />
-      <button v-on:click="addTodo" :disabled="!newTitle">Add</button>
+      <input type="text" v-model="newTitle" placeholder="New todo title" aria-label="=New todo title"/>
+      <button v-on:click="todoStore.addTodo(newTitle)" :disabled="!newTitle">Add</button>
     </div>
   </section>
 
